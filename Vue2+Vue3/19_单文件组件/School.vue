ba303e8737a3c42d6efe0e0475ebe6ebcc87ef38
Gaara <578045349@qq.com>
2025-07-19 02:23:26 +0800

    18-19

---
+++ Vue2+Vue3/19_单文件组件/School.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h2>学校名称：{{schoolName}}</h2>
+        <h2>学校地址：{{address}}</h2>
+        <button @click="showName">点击提示学校名</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            schoolName: '上海大学',
+            address: '上海'
+        }
+    },
+    methods: {
+        showName() {
+            alert(this.schoolName)
+        }
+    }
+}
+</script>
+
+<style>
+    .demo {
+        border-block-color: red;
+    }
+</style>
